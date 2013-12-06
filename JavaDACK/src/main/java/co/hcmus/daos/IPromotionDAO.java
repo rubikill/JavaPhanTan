1009cb3b1e389cd225a6c8a31f778d5320ddf9ac
blackvirus <ndtoan14592@gmail.com>
2013-12-07 02:44:42 +0700

    IpromotionDAO, PromotionDetailDAO

---
+++ JavaDACK/src/main/java/co/hcmus/daos/IPromotionDAO.java
@@ -0,0 +1,14 @@
+package co.hcmus.daos;
+
+import java.util.List;
+
+import co.hcmus.models.Promotion;
+
+public interface IPromotionDAO {
+	public void addPromotion(Promotion promotion);
+	public void updatePromotion(Promotion promotion);
+	public void getPromotionById(String id);
+	public void deletePromotion(String id);
+	public List<Promotion> getPromotions();
+
+}
---
+++ JavaDACK/src/main/java/co/hcmus/daos/Imp/PromotionDAOMongo.java
@@ -0,0 +1,52 @@
+package co.hcmus.daos.Imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import co.hcmus.daos.IPromotionDAO;
+import co.hcmus.models.Promotion;
+
+public class PromotionDAOMongo implements IPromotionDAO{
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	public static final String COLLECTION_NAME = "promotion";
+	
+	public PromotionDAOMongo()
+	{
+		if (!mongoTemplate.collectionExists(Promotion.class)) {
+			mongoTemplate.createCollection(Promotion.class);
+		}
+	}
+	@Override
+	public void addPromotion(Promotion promotion) {
+		// TODO Auto-generated method stub
+		mongoTemplate.insert(promotion, COLLECTION_NAME);
+	}
+
+	@Override
+	public void updatePromotion(Promotion promotion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPromotionById(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePromotion(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Promotion> getPromotions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
