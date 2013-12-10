6e94a66cb3cb281e7759881fedf0b4063fdd3c51
Thanh Toan <httoan@tech-arts.com>
2013-12-10 11:01:29 +0700

    add send mail helper, forgotpassword

--- JavaDACK/src/main/java/co/hcmus/util/Tools.java
+++ JavaDACK/src/main/java/co/hcmus/util/Tools.java
@@ -5,14 +5,20 @@ import java.util.Collection;
 import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
 public class Tools {
+
+    
 	public static <T> T fromJsonTo(String json, Class<T> valueType) {
-        return new JSONDeserializer<T>().use(null,valueType).deserialize(json);
-		
+        return new JSONDeserializer<T>().use(null, valueType).deserialize(json);
+
     }
-	public static <T> String toJsonArray(Collection<T> collection) {
+
+
+    public static <T> String toJsonArray(Collection<T> collection) {
         return new JSONSerializer().exclude("*.class").serialize(collection);
     }
-	public static <T> String toJson(Class<T> valueType) {
-        return new JSONSerializer().exclude("*.class").serialize(valueType);
+
+
+    public static <T> String toJson(Object value) {
+        return new JSONSerializer().serialize(value);
     }
 }
